051335461dfee59793aec5744e8d84aeac5c64e6
Robert Ackley <ackleyrc@gmail.com>
2018-02-04 10:27:32 -0800

    Add demo functionality to display line drawing on click-and-drag

--- Assets/Scripts/DemoManager.cs
+++ Assets/Scripts/DemoManager.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DemoManager : MonoBehaviour {
-
-    public GameObject hexPrefab;
-    public GameObject highlightPrefab;
-
-    GameObject highlight;
-
+public class DemoManager : MonoBehaviour
+{
     public int width;
     public int height;
 
     public Text widthText;
     public Text heightText;
 
+    public GameObject hexPrefab;
+    public GameObject highlightPrefab;
+
+    List<GameObject> highlights = new List<GameObject>();
+
     HexGrid grid;
     Dictionary<Cube, GameObject> hexes = new Dictionary<Cube, GameObject>();
 
+    Cube firstCube;
     Cube previousCube = new Cube(0, 0 ,0);
 
     CoordDisplay currentCoordDisplay = CoordDisplay.Cube;
@@ -27,29 +28,64 @@ public class DemoManager : MonoBehaviour {
     {
         SetGrid();
 
-        highlight = GameObject.Instantiate(highlightPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        GameObject highlight = GameObject.Instantiate(highlightPrefab, new Vector3(0, 0, 0), Quaternion.identity);
         highlight.SetActive(false);
+        highlights.Add(highlight);
     }
 
     private void Update()
     {
+        // Determine which hex the mouse is currently hovering over
         Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Cube nearestCube = grid.PixelToCube(worldPoint.x, worldPoint.y, 1f);
+        Cube cubeUnderMouse = grid.PixelToCube(worldPoint.x, worldPoint.y, 1f);
 
-        if (nearestCube != previousCube)
+        // When left mouse button first clicked, store off hex location under mouse
+        if (Input.GetMouseButtonDown(0))
         {
-            highlight.transform.position = grid.CubeToPixel(nearestCube, 1f) + Vector3.forward;
-            previousCube = nearestCube;
-            highlight.SetActive(grid.GetHexes().Contains(nearestCube));
+            firstCube = cubeUnderMouse;
         }
-
-        if (Input.GetMouseButtonUp(0))
+        // Otherwise, if left mouse button is being held down (dragging state) over grid...
+        else if (Input.GetMouseButton(0))
         {
-            if (hexes.ContainsKey(nearestCube))
+            // ... and the mouse is under a new hex location...
+            if (cubeUnderMouse != firstCube && cubeUnderMouse != previousCube)
             {
-                Debug.Log("Cube: " + nearestCube);
+                // Get the hex locations that form a line between where the mouse was first clicked and where it is now
+                List<Cube> cubesInLine = Cube.LineDraw(firstCube, cubeUnderMouse);
+                // Relocate or create highlights under each hex in the line
+                for (int i = 0; i < cubesInLine.Count; i++)
+                {
+                    if (i >= highlights.Count)
+                    {
+                        GameObject highlight = GameObject.Instantiate(highlightPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                        highlights.Add(highlight);
+                    }
+                    highlights[i].transform.position = grid.CubeToPixel(cubesInLine[i], 1f) + Vector3.forward;
+                    highlights[i].SetActive(grid.GetHexes().Contains(cubeUnderMouse));
+                }
+                // Deactivate any additional highlights beyond the number needed to highlight the line
+                for (int j = cubesInLine.Count; j < highlights.Count; j++)
+                {
+                    highlights[j].SetActive(false);
+                }
             }
         }
+        // Otherwise, if left mouse button has been released or is not held and a new hex has been moused over...
+        else if (Input.GetMouseButtonUp(0) || cubeUnderMouse != previousCube)
+        {
+            // Set the first highlight location to the hex currently under the mouse
+            highlights[0].transform.position = grid.CubeToPixel(cubeUnderMouse, 1f) + Vector3.forward;
+            highlights[0].SetActive(grid.GetHexes().Contains(cubeUnderMouse));
+
+            // Deactivate any additional highlights beyond the number needed to highlight the current hex
+            for (int i = 1; i < highlights.Count; i++)
+            {
+                highlights[i].SetActive(false);
+            }
+        }
+
+        // Make note of the hex currently under the mouse this frame for comparison next frame
+        previousCube = cubeUnderMouse;
     }
 
     public void SetGrid()
